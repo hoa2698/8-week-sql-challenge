69191b9a9809187f43145dcb6f500587c53e88c3
An Hoa Nguyen <sally.hoanguyen2698@gmail.com>
2022-10-04 19:17:06 +1030

    Add q2 to q6

--- week_2/solution.sql
+++ week_2/solution.sql
@@ -44,3 +44,46 @@ ALTER COLUMN distance TYPE FLOAT USING (distance::FLOAT);
 -- Question 1
 SELECT COUNT(customer_id)
 FROM customer_orders_updated;
+
+-- Question 2
+SELECT COUNT(DISTINCT order_id)
+FROM customer_orders_updated;
+
+SELECT runner_id, COUNT(order_id) as successful_order
+FROM runner_orders_updated
+WHERE cancellation is NULL
+GROUP BY runner_id;
+
+-- Question 3
+SELECT runner_id, count(order_id)
+FROM runner_orders_updated
+WHERE cancellation is NULL
+GROUP BY runner_id;
+
+-- Question 4
+SELECT pizza_names.pizza_name, COUNT(customer_orders_updated.pizza_id) as delivered_pizza_typed
+FROM customer_orders_updated
+JOIN pizza_runner.pizza_names
+ON pizza_names.pizza_id = customer_orders_updated.pizza_id
+JOIN runner_orders_updated
+ON runner_orders_updated.order_id = customer_orders_updated.order_id
+WHERE runner_orders_updated.cancellation is NULL 
+GROUP BY pizza_names.pizza_name;
+
+-- Question 5
+SELECT customer_orders_updated.customer_id, pizza_names.pizza_name, count(pizza_names.pizza_name) as pizza_count
+FROM customer_orders_updated
+JOIN pizza_runner.pizza_names
+ON customer_orders_updated.pizza_id = pizza_names.pizza_id
+GROUP BY customer_orders_updated.customer_id, pizza_names.pizza_name
+ORDER BY customer_orders_updated.customer_id;
+
+-- Question 6
+SELECT customer_orders_updated.order_id, COUNT(customer_orders_updated.pizza_id) as pizza_count
+FROM customer_orders_updated 
+INNER JOIN runner_orders_updated
+ON customer_orders_updated.order_id = runner_orders_updated.order_id
+WHERE runner_orders_updated.cancellation IS NULL
+GROUP BY customer_orders_updated.order_id
+ORDER BY pizza_count DESC
+LIMIT 1;
